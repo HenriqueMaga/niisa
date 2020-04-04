457e8d4b2f88a6d8e7b86bf5cd2be76fe300b142
HenriqueMaga <henrique.ma400@gmail.com>
2020-04-04 14:42:38 -0300

    inserindo produtos na base do atlas

--- scr/controllers/ProductController.js
+++ scr/controllers/ProductController.js
@@ -4,17 +4,19 @@ module.exports = {
     async create(request, response) {    
         try {
             const product = await Product.create(request.body);
-    
+            const { name, type, value } = request.body;
+
+            console.log({ name, type, value });
             return response.send({ product });
         } catch (err) {
             return response.status(400).send({ error: 'Insert Failed' });
         }
-    },/*
+    },
 
     async index (request, response) {
         console.log('Vou pesquisar produtos');
         const products = await connection.Query() ;
         return response.json(products);
-    }*/
+    }
 
 }
