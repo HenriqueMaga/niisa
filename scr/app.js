cdc492750b562d4dbc7bb9f550ab5f7b9318e43d
HenriqueMaga <henrique.ma400@gmail.com>
2020-04-04 14:39:32 -0300

    Removido body parse das dependências

--- scr/app.js
+++ scr/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.json());
 app.use(routes);
-app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
     response.send('Hello World!');
